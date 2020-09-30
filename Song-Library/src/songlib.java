8b4aecf132995611eaf935968eebc51241db30dd
nadaelshamaa <nada.elshamaa@rutgers.edu>
2020-09-30 19:31:06 -0400

    File setup

--- Song-Library/src/songlib.java
+++ Song-Library/src/songlib.java
@@ -1,4 +1,15 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class songlib {
-	
+public class songlib extends Application {
+
+	public void start (Stage primaryStage) {
+		primaryStage.setTitle("Song Library");
+		primaryStage.setResizable(false);
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		launch(args);
+	}
+
 }
