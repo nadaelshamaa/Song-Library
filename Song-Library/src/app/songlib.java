4cb3821a6f85f2056bf1132b51ad0da3a70945eb
nadaelshamaa <nada.elshamaa@rutgers.edu>
2020-10-01 00:24:16 -0400

    Finishing design.fxml

---
+++ Song-Library/src/app/songlib.java
@@ -0,0 +1,27 @@
+package app;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class songlib extends Application {
+
+	public void start (Stage primaryStage) throws Exception{
+		FXMLLoader loader = new FXMLLoader();   
+		loader.setLocation(
+				getClass().getResource("/view/design.fxml"));
+		AnchorPane root = (AnchorPane)loader.load();
+
+		Scene scene = new Scene(root);
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Song Library");
+		primaryStage.setResizable(false);
+		primaryStage.show();
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		launch(args);
+	}
+
+}
---
+++ Song-Library/src/view/eventhandler.java
@@ -0,0 +1,22 @@
+package view;
+
+public class eventhandler {
+
+	public void add() {
+		//pop up with the details
+		//check for duplicates
+		//add it to the file in order and the display
+	}
+	
+	public void delete() {
+		//delete the selected song
+		//remove it from file
+		//select the next song if there is one otherwise previous
+	}
+	
+	public void edit() {
+		
+	}
+	
+
+}
